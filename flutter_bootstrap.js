fb82b812077df2b2bd79359f90c07d8e1719d0f8
namseokyoo <namseok.yoo@gmail.com>
2024-10-30 21:11:59 +0900

    modify

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "189205840"
+    serviceWorkerVersion: "1677349896"
   }
 });
